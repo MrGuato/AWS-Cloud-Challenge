6359187112582fe7f02149faf1f66b1ea642973d
Jonathan <111000657+MrGuato@users.noreply.github.com>
2025-03-22 18:29:27 -0400

    Update handler.js
    Signed-off-by: Jonathan <111000657+MrGuato@users.noreply.github.com>

--- backend/starwars-api/handler.js
+++ backend/starwars-api/handler.js
@@ -1,13 +1,14 @@
 const facts = require("./starwarsFacts");
 
-MediaSourceHandle.exports.starwarsfact = async (event) => {
-    const randomFact = facts[Math.floor(Math.random() * facts.length)];
-    return {
-        statsCode: 200,
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "https://cloud.mrcyberleon.org"
-        },
-        body: JSON.stringify({ fact: randomFact })
-    };
+module.exports.starwarsfact = async (event) => {
+  const randomFact = facts[Math.floor(Math.random() * facts.length)];
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "https://cloud.mrcyberleon.org"
+    },
+    body: JSON.stringify({ fact: randomFact })
+  };
 };
